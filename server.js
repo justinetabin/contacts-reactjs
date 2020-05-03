98fdcc2376781738d05b89cabca9f605a15d1dee
Justine Tabin <justinetabin@gmail.com>
2020-05-03 18:12:41 +0800

    Add file server

---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Path = require('path');
+
+const init = async () => {
+
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'build')
+      }
+    }
+  });
+
+  await server.register(require('@hapi/inert'));
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: {
+        file: 'index.html'
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: (request, h) => {
+        const { param } = request.params
+        var match = param.match(/\.([0-9a-z]+)(?:[\?#]|$)/i);
+        if (match) {
+            return h.file(param);
+        } else {
+            return h.file('index.html');
+        }
+    }
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+init();
